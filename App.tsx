ed945e878304f129aff9c35371d6d0e4b32a94bf
Kenneth Sanders <kennysandersschool@gmail.com>
2022-11-06 19:30:03 -0500

    Initial template setup

---
+++ App.tsx
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <WebView source={{uri: 'https://rewind-alpha.kensand.net'}}></WebView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
